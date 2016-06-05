c6b31908770c2b703aee073079ea704a89b9094b
“Brian <brianlens@gmail.com>
2016-06-05 14:15:03 +0200

    specified methods for testing

---
+++ spec/controllers/reservations_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+Rspec.describe ReservationsController, type: :controller do
+  describe "GET index" do
+
+  end
+
+  describe "GET show" do
+
+  end
+
+  describe "GET edit" do
+
+  end
+
+  describe "PATCH update" do
+
+  end
+
+  describe "POST create" do
+
+  end
+
+  describe "DELETE destroy" do
+
+  end
+
+  describe "GET new" do
+
+  end
+end
---
+++ spec/controllers/testimonials_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+Rspec.describe TestimonialsController, type: :controller do
+  describe "GET index" do
+
+  end
+
+  describe "GET show" do
+
+  end
+end
